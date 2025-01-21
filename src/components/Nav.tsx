c9b42cc1bb486f33caa8f99bb7af89f1d607a0ea
Peter Rudolf <peterrudolf249@gmail.com>
2025-01-21 17:34:38 -0500

    set up project with the starter code

---
+++ src/components/Nav.tsx
@@ -0,0 +1,20 @@
+const Nav = () => {
+  // TODO: Add necessary code to display the navigation bar and link between the pages
+  return (
+    <div>
+      <div>Nav</div>
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  )
+};
+
+export default Nav;
