eeee03345e039947ea9f31d7cbcc1585172e7be5
Peter Rudolf <peterrudolf249@gmail.com>
2025-01-27 19:50:34 -0500

    edit to vite.config.ts for render

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   envDir: './environment',
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+    open: true
+  }
 });
